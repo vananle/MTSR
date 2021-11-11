5dbe50a88e1f7178a44e54996af8c66c6f0596bd
anle <vananle1993@gmail.com>
2021-11-11 11:35:59 +0900

    add vae_sr

---
+++ vae_sr/var/train.py
@@ -0,0 +1,69 @@
+import os.path
+import sys
+
+import tensorflow as tf
+
+sys.path.append('..')
+
+import time
+import models
+import torch
+import utils
+
+import warnings
+from datetime import date
+from routing import *
+
+warnings.simplefilter("ignore")
+warnings.filterwarnings("ignore", category=UserWarning)
+
+
+def main(args, **model_kwargs):
+    device = torch.device(args.device)
+    args.device = device
+    if 'abilene' in args.dataset:
+        args.nNodes = 12
+        args.nLinks = 30
+        args.day_size = 288
+        scale = 100.0
+    elif 'geant' in args.dataset:
+        args.nNodes = 22
+        args.nLinks = 72
+        args.day_size = 96
+        scale = 100.0
+    elif 'brain' in args.dataset:
+        args.nNodes = 9
+        args.nLinks = 28
+        args.day_size = 1440
+    else:
+        raise ValueError('Dataset not found!')
+    logger = utils.Logger(args)
+    X_train, X_val, L_train, L_val, A_train, A_val = utils.load_traindata(args)
+
+    X_train = X_train / args.scale
+    X_val = X_val / args.scale
+
+    model = models.VAE(args=args, X_val=X_val)
+
+    model.compile(optimizer=tf.keras.optimizers.Adam())
+    print(model.encoder.summary())
+    print(model.decoder.summary())
+    if not args.test:
+        history = model.fit(X_train, epochs=args.epochs, batch_size=args.train_batch_size,
+                            callbacks=[utils.EarlyStoppingAtMinLoss()], )
+        model.encoder.save(os.path.join(args.log_dir, 'encoder'))
+        model.decoder.save(os.path.join(args.log_dir, 'decoder'))
+
+    test_traffic = utils.load_test_traffic(args)
+    graph = load_network_topology(args.dataset, args.datapath)
+    vae_ls2sr(test_traffic=test_traffic, vae=model, graph=graph, args=args)
+
+
+if __name__ == "__main__":
+    args = utils.get_args()
+    t1 = time.time()
+    main(args)
+    t2 = time.time()
+    mins = (t2 - t1) / 60
+    print("Total time spent: {:.2f} seconds".format(mins))
+    print('Date&Time: ', date.today())
